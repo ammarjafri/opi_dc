fe7c58e049dbc79e9497ad7ad73651bc2e40b73f
ammarjafri <ammarjafri3@gmail.com>
2022-06-13 13:33:20 +0500

    test5, complete test

--- cam_check.py
+++ cam_check.py
@@ -1,24 +1,26 @@
 import os,subprocess,time
-com = 'v4l2-ctl --list-devices | grep camera'
-com_1 = 'sudo systemctl restart motioneye'
+cams_c = 'v4l2-ctl --list-devices | grep camera'
+res_me = 'sudo systemctl restart motioneye'
 a=0
-#while True:
-#    try:
-check_cam = subprocess.Popen(com,shell=True,stdout=subprocess.PIPE).communicate()
-check_cam = check_cam[0]
-print(check_cam.decode())
-if check_cam.decode() == '':
-    print('found')
-#        for fi in check_cam:
-    #         if a==0:
-    #             pass
-    #     if a==1:
-    #         while True:
-    #             check_cam = subprocess.Popen(com,shell=True,stdout=subprocess.PIPE).communicate()
-    #             if a==0:
-    #                 os.system(com_1)
-    #                 time.sleep(1)
-    #                 break
-    #     time.sleep(1)
-    # except:
-    #     pass
+while True:
+    try:
+        check_cam = subprocess.Popen(cams_c,shell=True,stdout=subprocess.PIPE).communicate()
+        check_cam = check_cam[0]
+        # print(check_cam.decode())
+        if check_cam.decode() == '':
+            print('camera disconnect')            
+            while True:
+                try:
+                    re_check_cam = subprocess.Popen(cams_c,shell=True,stdout=subprocess.PIPE).communicate()
+                    if not re_check_cam.decode() == '':
+                        print('camera connect again, restarting motioneye')
+                        time.sleep(1)
+                        os.system(res_me)
+                        time.sleep(1)
+                        break
+                    time.sleep(1)
+                except:
+                    pass
+        time.sleep(1)
+    except:
+        pass
